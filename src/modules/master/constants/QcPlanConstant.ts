1ebfcbec5238aaadaf3b866d3d15677b1e41ba39
gigihprasetia <82494987+gigihprasetia@users.noreply.github.com>
2025-09-17 21:15:02 +0700

    qcplan

---
+++ src/modules/master/constants/QcPlanConstant.ts
@@ -0,0 +1,10 @@
+import type { TableColumnType } from "@/components/tables/Table.vue";
+
+export const ColumnsQcPlan: TableColumnType[] = [
+  {
+    key: "name",
+    label: "Nama",
+    align: "left",
+    sort: false,
+  },
+];
